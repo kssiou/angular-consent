d66ca8e9dcf1ea5925018d6798ef87e916c50ecb
kssiou <mouadkssiou@gmail.com>
2022-06-08 23:36:55 +0000

    13 commit

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -8,6 +8,7 @@ export class dataService {
 layout_gui='box';
 position_x_gui='right';
 position_y_gui='bottom';
+title_consent_modal='je suis';
 transition_gui='';
 layout_settings='';
 position_settings='';
