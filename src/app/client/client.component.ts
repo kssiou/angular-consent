979c02fdd90dbc25bfd54b6b80c0296f941f39b8
hajarEchcherki <hajarech7@gmail.com>
2022-06-11 20:53:00 +0200

    19 commit

--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { dataService } from '../data.service';
+import { DataService } from '../service/data.service';
+import { Client } from 'src/app/client';
 
 @Component({
   selector: 'app-client',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client.component.scss']
 })
 export class ClientComponent implements OnInit {
+  client=new Client;
+  clients: any;
 
-  constructor() { }
+  constructor(public dataservice:DataService) { }
+  insertData(){
+    //console.log('bonjour-insertion-test');
+    //console.log(this.product);
+    this.dataservice.insertData(this.client).subscribe(res =>{
+      //console.log(res);
+      this.getClientData();
+    })
+  
+  }getClientData() {
 
+    console.log('liste des clients');
+//    console.log(this.position_x_gui)
+    this.dataservice.getData().subscribe(res =>{
+      console.log(res);
+      this.clients=res;
+    }
+      )
+  }
   ngOnInit(): void {
+     this.getClientData();
   }
 
 }
